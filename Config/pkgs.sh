f81f551070b0bd643e4d6d3f37c9f92eda2806a0
ilyann gwinner <ilyann.gwinner@gmail.com>
2024-11-08 23:12:51 +0100

    polybar 2

--- Config/pkgs.sh
+++ Config/pkgs.sh
@@ -1,10 +1,18 @@
+# Channel update
 nix-channel --add https://nixos.org/channels/nixpkgs-unstable
 nix-channel --update
 
+# Package installation
 nix-env -iA nixpkgs.picom
 nix-env -iA nixpkgs.polybar
 nix-env -iA nixpkgs.cmatrix
 
+# Font installation
+nix-env -iA nixpkgs.siji
+nix-env -iA nixpkgs.noto-fonts
+nix-env -iA nixpkgs.ttf-dejavu
+
+# Start configs
 config=~/afs/.confs/config
 
 picom --config $config/picom/picom.conf &
