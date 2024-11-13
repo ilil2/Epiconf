1e2cd488b70ba956fc70a388060f58be05ea1de4
Ilyann Gwinner <ilyann.gwinner@epita.fr>
2024-11-13 20:39:22 +0100

    i3lock with workspaces

---
+++ Config/config/i3/i3lock.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+WORK=$(i3-msg -t get_workspaces | grep -o '"name":"[^"]*".*"focused":true' | rev |cut -d '"' -f 6 | rev)
+i3-msg workspace number 10
+alacritty -e cmatrix & sleep 0.2
+i3-msg fullscreen toggle
+i3lock &
+while pgrep -x 'i3lock' > /dev/null
+do
+	sleep 0.2
+done
+i3-msg kill
+echo $WORK
+i3-msg workspace number "$WORK"
