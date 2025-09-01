9dbc2692ff08679006994a1ad77d8c8b1409e6af
Ilyann Gwinner <ilyann.gwinner@epita.fr>
2025-09-01 16:49:44 +0200

    patch i3lock et add passwd protection

--- Config/config/i3/i3lock.sh
+++ Config/config/i3/i3lock.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
 
 WORK=$(i3-msg -t get_workspaces | grep -o '"name":"[^"]*".*"focused":true' | rev |cut -d '"' -f 6 | rev)
-i3-msg workspace number 10
 alacritty -e cmatrix & sleep 0.2
+i3-msg move workspace number 10
+i3-msg workspace number 10
 i3-msg fullscreen toggle
 i3lock &
 while pgrep -x 'i3lock' > /dev/null
@@ -10,5 +11,4 @@ do
 	sleep 0.2
 done
 i3-msg kill
-echo $WORK
 i3-msg workspace number "$WORK"
--- setup.sh
+++ setup.sh
@@ -1,6 +1,24 @@
-config=~/afs/.confs
-cp -r Config/* $config
+hash_sha256() {
+	local input="$1"
+	echo -n "$input" | sha256sum | cut -d' ' -f1
+}
 
-i3-msg reload
-source $config/*
-vim +PluginInstall +qall
+read -sp "Password         : " PASSWORD
+echo
+read -sp "Confirm Password : " CONFIRM
+echo
+
+if [[ "$PASSWORD" == "$CONFIRM" ]]; then
+	config=~/afs/.confs
+	cp -r Config/* $config
+
+	HASH_PASSWORD=$(hash_sha256 "$PASSWORD")
+	sed -i "s/SECURE_PASSWORD/$HASH_PASSWORD/" "$config/bashrc"
+
+	i3-msg reload
+	source $config/*
+	vim +PluginInstall +qall
+
+else
+	echo "Wrong Password"
+fi
