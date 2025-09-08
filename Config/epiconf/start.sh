770a5f4f90b5fdd438b3633facf39faf4d3b7e66
Ilyann Gwinner <ilyann.gwinner@gmail.com>
2025-09-08 14:37:50 +0200

    config 2

--- Config/epiconf/start.sh
+++ Config/epiconf/start.sh
@@ -4,7 +4,7 @@ fichier="config"
 
 while IFS= read -r line; do
     export $line
-    var_name=$(cut $line -d'=' -f1)
+    var_name=$(cut $line -d'=' -f1 | tr '[:upper:]' '[:lower:]')
     var_value=$(cut $line -d'=' -f2)
     i3-msg set \$$var_name $var_value
 done < "$fichier"
