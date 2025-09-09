88c1f9db6000e01bc9a95c2934a84f9a16037d8c
Ilyann Gwinner <ilyann.gwinner@gmail.com>
2025-09-09 09:17:47 +0200

    patch start.sh

--- Config/epiconf/start.sh
+++ Config/epiconf/start.sh
@@ -1,11 +1,18 @@
 #!/bin/sh
 
 file=~/afs/.confs/epiconf/config
+i3conf=~/afs/.confs/epiconf/i3_vars.conf
+
+: > "$i3conf"
 
 while IFS= read -r line; do
-    export "$line"
+    [ -z "$line" ] && continue
     var_name=$(cut -d'=' -f1 <<< "$line" | tr '[:upper:]' '[:lower:]')
-    var_value=$(cut -d'=' -f2 <<< "$line")
-	echo "$line"
-    i3-msg "set \$$var_name $var_value"
+    var_value=$(cut -d'=' -f2- <<< "$line")
+
+    export "$var_name=$var_value"
+
+    echo "set \$$var_name $var_value" >> "$i3conf"
 done < "$file"
+
+i3-msg reload
