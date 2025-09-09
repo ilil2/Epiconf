be0fa3895f50a1f5d0bbbbe9dd05751673600322
Ilyann Gwinner <ilyann.gwinner@epita.fr>
2025-09-09 08:51:57 +0200

    unstable

--- Config/epiconf/start.sh
+++ Config/epiconf/start.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
 
-fichier="config"
+file=~/afs/.confs/epiconf/config
 
 while IFS= read -r line; do
-    export $line
-    var_name=$(cut $line -d'=' -f1 | tr '[:upper:]' '[:lower:]')
-    var_value=$(cut $line -d'=' -f2)
-    i3-msg set \$$var_name $var_value
-done < "$fichier"
+    export "$line"
+    var_name=$(cut -d'=' -f1 <<< "$line" | tr '[:upper:]' '[:lower:]')
+    var_value=$(cut -d'=' -f2 <<< "$line")
+	echo "$line"
+    i3-msg "set \$$var_name $var_value"
+done < "$file"
