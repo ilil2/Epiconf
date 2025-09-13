6b018b193a25509daf8c8549909f8221ee080d10
Ilyann Gwinner <ilyann.gwinner@gmail.com>
2025-09-13 12:11:48 +0200

    change start.sh for del coms

--- Config/epiconf/start.sh
+++ Config/epiconf/start.sh
@@ -1,16 +1,40 @@
-#!/bin/sh
+#!/bin/bash
 
 file=~/afs/.confs/epiconf/config.ini
 
 section=""
-while IFS='= ' read -r key value; do
-    if [[ $key =~ ^\[(.*)\]$ ]]; then
+while IFS= read -r line || [ -n "$line" ]; do
+    # Trim espaces début/fin
+    line="${line#"${line%%[![:space:]]*}"}"   # supprime espaces début
+    line="${line%"${line##*[![:space:]]}"}"   # supprime espaces fin
+
+    # Ignore lignes vides ou commentaires
+    [[ -z "$line" || "$line" =~ ^[;\#] ]] && continue
+
+    # Section [XXX]
+    if [[ $line =~ ^\[(.*)\]$ ]]; then
         section="${BASH_REMATCH[1]}"
-    elif [[ -n $key && $key != \#* && $key != \;* ]]; then
-        var=$key  #"${section}_${key}"
+        continue
+    fi
+
+    # Clé = valeur
+    if [[ $line =~ ^([^=]+)=(.*)$ ]]; then
+        key="${BASH_REMATCH[1]}"
+        value="${BASH_REMATCH[2]}"
+
+        # Trim espaces clé et valeur
+        key="${key%"${key##*[![:space:]]}"}"
+        key="${key#"${key%%[![:space:]]*}"}"
+        value="${value#"${value%%[![:space:]]*}"}"
+        value="${value%"${value##*[![:space:]]}"}"
+
+        # Si tu veux inclure le nom de la section dans la variable :
+        # var="${section}_${key}"
+        var="$key"
+
         declare "$var=$value"
         export "$var"
     fi
-done < $file
+done < "$file"
 
 i3-msg reload
