915f7e21c7b8fb061ff54b53120351bdf6b1663f
Ilyann Gwinner <ilyann.gwinner@gmail.com>
2025-09-17 11:12:48 +0200

    add change time

--- Config/config/i3/start_service.sh
+++ Config/config/i3/start_service.sh
@@ -1,13 +1,12 @@
 #!/bin/sh
 
-i3-nagbar -t warning -m 'You pressed the exit shortcut. Do you really want to exit i3? This will end your X session.'
-# Line 212: Wallpaper loop
+# Wallpaper loop
 while true; do
     feh --randomize --bg-fill ~/afs/.confs/config/wallpapers
     sleep $WALLPAPER_TIME
 done &
 
-# Line 213: Inactivity lock
+# Inactivity lock
 while true; do
     if [ $(nix-shell -p xprintidle --command xprintidle) -ge $INACTIVITY_TIME ]; then
         chmod +x ~/afs/.confs/config/i3/i3lock.sh
@@ -16,7 +15,12 @@ while true; do
     sleep 1
 done &
 
-# Line 214: Start scripts
+while true; do
+    sleep 5
+    source ~/afs/.confs/epiconf/start.sh
+done &
+
+# Start scripts
 for file in ~/afs/.confs/epiconf/start_script/*.sh; do
     if [ -f "$file" ] && [ -x "$file" ]; then
         sh "$file" &
