c87c27cae6af24b903b38a3f0f003195c871f9dc
GWINNER Ilyann <108801514+ilil2@users.noreply.github.com>
2025-09-22 14:49:43 +0200

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -1,9 +1,8 @@
 git clone https://github.com/ilil2/My-Epita-Confs.git ~/epiconf
-cd ~/epiconf
-chmod +x setup.sh
-echo "chmod"
-./setup.sh
-cd
-echo "rm"
+(
+  cd ~/epiconf || exit 1
+  chmod +x setup.sh
+  ./setup.sh
+)
 rm -rf ~/epiconf
 echo "rm"
