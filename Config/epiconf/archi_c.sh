f18b2667c0561c78c497ff4ee835252e23421da3
Corentin Del Pozo <corentin.del-pozo@r04p18.midlab.kb.sm.cri.epita.fr>
2025-09-29 13:31:03 +0200

    add command carchi

---
+++ Config/epiconf/archi_c.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+if [ $# -ne 1 ]; then
+	echo "only one arg pls"
+	exit 1
+fi
+
+mkdir "$1"
+cd "$1"
+touch "$1.c"
+touch "$1.h"
+
+UPPER=${1^^}
+H="_H"
+
+echo -e "#ifndef $UPPER$H\n#define $UPPER$H\n\n#endif /* ! $UPPER$H */" > "$1.h"
+echo -e "#include \"$1.h\"\n" > "$1.c"
