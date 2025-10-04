5e4aaf5d9e029260e3992ebaba10e726ffbedaef
Corentin Del Pozo <corentin.del-pozo@r04p19.midlab.kb.sm.cri.epita.fr>
2025-10-04 09:52:42 +0200

    install too

--- Config/install.sh
+++ Config/install.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-dot_list="bashrc config emacs gitconfig gitignore jnewsrc mozilla msmtprc muttrc signature slrnrc ssh thunderbird vim vimrc Xdefaults"
+dot_list="bashrc config emacs gitconfig gitignore jnewsrc mozilla msmtprc muttrc signature slrnrc ssh thunderbird vim vimrc Xdefaults gdbinit"
 
 for f in $dot_list; do
   rm -rf "$HOME/.$f"
