f9e68836dfc88df75e5155ce6aacc76fff7134a0
Otto Debrie <otto.debrie@r04p18.midlab.kb.sm.cri.epita.fr>
2025-10-21 13:43:44 +0200

    change .bin name to .trash

--- Config/epiconf/remove_to_bin.sh
+++ Config/epiconf/remove_to_bin.sh
@@ -1,10 +1,12 @@
+#!/bin/sh
+dir=~/.trash
 if [ $# -eq 0 ]; then
     echo "rm: missing operand"
 elif [ "$1" = "-r" ] || [ "$1" = "-f" ] || [ "$1" = "-rf" ]; then
     rm $@
 else
-    if [ ! -d ~/.bin ]; then
-        mkdir ~/.bin
+    if [ ! -d "$dir" ]; then
+        mkdir "$dir"
     fi
-    mv $@ ~/.bin
+    mv $@ "$dir"
 fi
