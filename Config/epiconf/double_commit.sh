bbfca1267ad1de1aeca5d1778a9011dc30e8485c
Otto Debrie <otto.debrie@r04p18.midlab.kb.sm.cri.epita.fr>
2025-11-03 13:25:54 +0100

    Added double-commit (dbcommit)

---
+++ Config/epiconf/double_commit.sh
@@ -0,0 +1,39 @@
+#!/bin/sh
+
+if [ "$1" = "--link" ] || [ "$1" = "-l" ]; then
+    if [ $# -ne 3 ]; then
+        echo "Invalid arguments: have $0 $1 'master' 'slave'"
+        exit 1
+    elif [ "$2" = "$3" ]; then
+        echo "Master cannot be slave"
+        exit 2
+    fi
+    echo "$(pwd)/$2;$(pwd)/$3" >> ~/afs/.link
+    echo "$2 and $3 successfully linked."
+    exit 0
+fi
+
+# Get other repo
+line=$(grep -e "$(pwd);*" ~/afs/.link)
+if [ "$line" = "" ]; then
+    echo "Repo not linked."
+    exit 1
+fi
+arr=(${line//;/ })
+repo=${arr[1]}
+
+# Commit and push om master
+echo "Pushing on master..."
+git add *
+git commit -m "$1"
+git push
+
+# Commit and push on slave
+echo "Pushing on slave..."
+cp * "$repo/"
+cd "$repo"
+git add "*"
+git commit -m "$1"
+git push
+
+echo "Done."
