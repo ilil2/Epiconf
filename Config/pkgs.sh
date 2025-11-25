444ebf408958ac4c3060ffced954c315be8f23ba
Ilyann Gwinner <ilyann.gwinner@gmail.com>
2025-11-25 14:37:58 +0100

    add: logs for installation

--- Config/pkgs.sh
+++ Config/pkgs.sh
@@ -1,30 +1,35 @@
 # Channel update
-nix-channel --add https://nixos.org/channels/nixpkgs-unstable
-nix-channel --update
+echo "Updating channel"
+nix-channel --add https://nixos.org/channels/nixpkgs-unstable > /dev/null 2>&1
+nix-channel --update > /dev/null 2>&1
 
 # Clear Nix
-nix-collect-garbage -d
+echo "Clear garbage"
+nix-collect-garbage -d > /dev/null 2>&1
 
 # Priority package
-nix-env -iA nixpkgs.rofi
-nix-env -iA nixpkgs.rofi-power-menu
-nix-env -iA nixpkgs.rip2
-nix-env -iA nixpkgs.bat
+echo "Installing Priority packages"
+nix-env -iA nixpkgs.rofi > /dev/null 2>&1
+nix-env -iA nixpkgs.rofi-power-menu > /dev/null 2>&1
+nix-env -iA nixpkgs.rip2 > /dev/null 2>&1
+nix-env -iA nixpkgs.bat > /dev/null 2>&1
 
 # Package installation
-nix-env -iA nixpkgs.picom
-nix-env -iA nixpkgs.polybar
-nix-env -iA nixpkgs.cmatrix
-nix-env -iA nixpkgs.blueman
-nix-env -iA nixpkgs.mdcat
-nix-env -iA nixpkgs.xprintidle
-nix-env -iA nixpkgs.xnotify
-nix-env -iA nixpkgs.autotiling
+echo "Installing Other packages"
+nix-env -iA nixpkgs.picom > /dev/null 2>&1
+nix-env -iA nixpkgs.polybar > /dev/null 2>&1
+nix-env -iA nixpkgs.cmatrix > /dev/null 2>&1
+nix-env -iA nixpkgs.blueman > /dev/null 2>&1
+nix-env -iA nixpkgs.mdcat > /dev/null 2>&1
+nix-env -iA nixpkgs.xprintidle > /dev/null 2>&1
+nix-env -iA nixpkgs.xnotify > /dev/null 2>&1
+nix-env -iA nixpkgs.autotiling > /dev/null 2>&1
 
 # Music installation
 if [ "$MUSIC_LOADER" == "enable" ]; then
-    nix-env -iA nixpkgs.spotify
-    nix-env -iA nixpkgs.deezer-enhanced
+    echo "Installing music packages"
+    nix-env -iA nixpkgs.spotify > /dev/null 2>&1
+    nix-env -iA nixpkgs.deezer-enhanced > /dev/null 2>&1
 fi
 
 # Git-repositories installation
@@ -32,20 +37,23 @@ fi
 #make -C ~/.ble
 
 # Font installation
-nix-env -iA nixpkgs.siji
-nix-env -iA nixpkgs.noto-fonts
-nix-env -iA nixpkgs.ttf-dejavu
+echo "Installing fonts"
+nix-env -iA nixpkgs.siji > /dev/null 2>&1
+nix-env -iA nixpkgs.noto-fonts > /dev/null 2>&1
+nix-env -iA nixpkgs.ttf-dejavu > /dev/null 2>&1
+nix-env -iA nixpkgs.nerd-fonts.jetbrains-mono > /dev/null 2>&1
 
 # Kill all
-pkill polybar
+pkill polybar > /dev/null 2>&1
 
 # Start configs
+echo "Setup config packages"
 config=~/afs/.confs/config
 
-picom --config $config/picom/picom.conf &
-polybar --config=$config/polybar/config.ini $POLYBAR_NAME &
-autotiling
+picom --config $config/picom/picom.conf > /dev/null 2>&1 &
+polybar --config=$config/polybar/config.ini $POLYBAR_NAME > /dev/null 2>&1 &
+autotiling > /dev/null 2>&1
 
-~/.xinitrc
+~/.xinitrc > /dev/null 2>&1
 sleep 0.5
 echo "Epiconf is setup" > /tmp/xnotify.fifo
