f8097fd574f4245e59227012d213d751449b9a05
Ilyann gwinner <ilyann.gwinner@epita.fr>
2026-04-28 17:33:59 +0200

    add: bash history in .confs
    Make bash history file session shared

--- Config/install.sh
+++ Config/install.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
 
-dot_list="bashrc config emacs gitconfig gitignore jnewsrc mozilla msmtprc muttrc signature slrnrc ssh thunderbird vim vimrc Xdefaults gdbinit blerc xinitrc"
+if ! test -f $AFS_DIR/.confs/bash_history; then
+    touch $AFS_DIR/.confs/bash_history
+fi
+
+dot_list="bashrc config emacs gitconfig gitignore jnewsrc mozilla msmtprc muttrc signature slrnrc ssh thunderbird vim vimrc Xdefaults gdbinit blerc bash_history"
 
 for f in $dot_list; do
   rm -rf "$HOME/.$f"
