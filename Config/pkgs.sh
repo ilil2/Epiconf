4d804547830b4a216bda435a2314138cc04b1d7b
Otto303 <ottodebrie@gmail.com>
2026-04-30 13:51:41 +0200

    add: colour-valgrind

--- Config/pkgs.sh
+++ Config/pkgs.sh
@@ -6,30 +6,30 @@ if [ "$1" != "setup" ]; then
     # Priority package
     echo -ne "Installing Priority packages \e[7C"
     BLA::start_loading_animation "${BLA_modern_metro[@]}"
-    nix profile add nixpkgs\#rofi > /dev/null 2>&1
-    nix profile add nixpkgs\#rip2 > /dev/null 2>&1
-    nix profile add nixpkgs\#bat > /dev/null 2>&1
-    nix profile add nixpkgs\#neovim > /dev/null 2>&1
+    nix profile install nixpkgs\#rofi > /dev/null 2>&1
+    nix profile install nixpkgs\#rip2 > /dev/null 2>&1
+    nix profile install nixpkgs\#bat > /dev/null 2>&1
+    nix profile install nixpkgs\#neovim > /dev/null 2>&1
     BLA::stop_loading_animation
 
     # Package installation
     echo -ne "Installing Other packages \e[7C"
     BLA::start_loading_animation "${BLA_modern_metro[@]}"
-    nix profile add nixpkgs\#picom > /dev/null 2>&1
-    nix profile add nixpkgs\#polybar > /dev/null 2>&1
-    nix profile add nixpkgs\#mdcat > /dev/null 2>&1
-    nix profile add nixpkgs\#dunst > /dev/null 2>&1
-    nix profile add nixpkgs\#xprintidle > /dev/null 2>&1
-    nix profile add nixpkgs\#autotiling > /dev/null 2>&1
-    nix profile add nixpkgs\#screen > /dev/null 2>&1
+    nix profile install nixpkgs\#picom > /dev/null 2>&1
+    nix profile install nixpkgs\#polybar > /dev/null 2>&1
+    nix profile install nixpkgs\#mdcat > /dev/null 2>&1
+    nix profile install nixpkgs\#dunst > /dev/null 2>&1
+    nix profile install nixpkgs\#xprintidle > /dev/null 2>&1
+    nix profile install nixpkgs\#autotiling > /dev/null 2>&1
+    nix profile install nixpkgs\#screen > /dev/null 2>&1
     BLA::stop_loading_animation
 
     # Music installation
     if [ "$MUSIC_LOADER" == "enable" ]; then
         echo -ne "Installing Music packages \e[7C"
         BLA::start_loading_animation "${BLA_modern_metro[@]}"
-        timeout 1m nix profile add nixpkgs\#spotify > /dev/null 2>&1
-        timeout 1m nix profile add nixpkgs\#deezer-enhanced > /dev/null 2>&1
+        timeout 1m nix profile install nixpkgs\#spotify > /dev/null 2>&1
+        timeout 1m nix profile install nixpkgs\#deezer-enhanced > /dev/null 2>&1
         BLA::stop_loading_animation
     fi
 
@@ -40,8 +40,21 @@ if [ "$1" != "setup" ]; then
     # Font installation
     echo -ne "Installing fonts \e[7C"
     BLA::start_loading_animation "${BLA_modern_metro[@]}"
-    nix profile add nixpkgs\#siji > /dev/null 2>&1
-    # nix profile add nixpkgs\#nerd-fonts.jetbrains-mono > /dev/null 2>&1
+    nix profile install nixpkgs\#siji > /dev/null 2>&1
+    # nix profile install nixpkgs\#nerd-fonts.jetbrains-mono > /dev/null 2>&1
+    BLA::stop_loading_animation
+
+    # Pip packages installation
+    echo -ne "Setting up python environment \e[7C"
+    BLA::start_loading_animation "${BLA_modern_metro[@]}"
+    python3 -m venv /tmp/vgenv &> /dev/null
+    sed -i 's/PS1=.*//g' /tmp/vgenv/bin/activate &> /dev/null
+    source /tmp/vgenv/bin/activate &> /dev/null
+    BLA::stop_loading_animation
+
+    echo -ne "Installing pip packages \e[7C"
+    BLA::start_loading_animation "${BLA_modern_metro[@]}"
+    pip install colour-valgrind &> /dev/null
     BLA::stop_loading_animation
 fi
 
