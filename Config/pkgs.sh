43e2e43c85097533ff15018529dd1bf3b721de92
GWINNER Ilyann <108801514+ilil2@users.noreply.github.com>
2026-05-29 09:27:52 +0200

    Fix packages (#12)
    * fix: rip
    
    Remove rip2 package and remove rm and mr aliases
    
    * fix: fonts
    
    Add font jetbrains mono (for nvim)

--- Config/pkgs.sh
+++ Config/pkgs.sh
@@ -7,7 +7,6 @@ if [ "$1" != "setup" ]; then
     echo -ne "Installing Priority packages \e[7C"
     BLA::start_loading_animation "${BLA_modern_metro[@]}"
     nix profile install nixpkgs\#rofi > /dev/null 2>&1
-    nix profile install nixpkgs\#rip2 > /dev/null 2>&1
     nix profile install nixpkgs\#bat > /dev/null 2>&1
     nix profile install nixpkgs\#neovim > /dev/null 2>&1
     BLA::stop_loading_animation
@@ -41,7 +40,7 @@ if [ "$1" != "setup" ]; then
     echo -ne "Installing fonts \e[7C"
     BLA::start_loading_animation "${BLA_modern_metro[@]}"
     nix profile install nixpkgs\#siji > /dev/null 2>&1
-    # nix profile install nixpkgs\#nerd-fonts.jetbrains-mono > /dev/null 2>&1
+    nix profile install nixpkgs\#nerd-fonts.jetbrains-mono > /dev/null 2>&1
     BLA::stop_loading_animation
 
     # Pip packages installation
