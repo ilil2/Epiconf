493526c90fb2faaa7ab1011d3d7e419482b14d9f
ilyann.gwinner <ilyann.gwinner@gmail.com>
2026-05-29 16:46:24 +0200

    fix: setup script
    Fix setup script

--- setup.sh
+++ setup.sh
@@ -1,50 +1,9 @@
-hash_sha256() {
-	local input="$1"
-	echo -n "$input" | sha256sum | cut -d' ' -f1
-}
-
 config=~/afs/.confs
-password_file=$config/.user
-
-if [[ -f $password_file ]]; then
-	sleep 0.5
-	read -sp "Password : " PASSWORD
-	HASH_PASSWORD=$(hash_sha256 "$PASSWORD")
-	SECURE_PASSWORD=$(cat $config/.user)
-	if [[ "$HASH_PASSWORD" == "$SECURE_PASSWORD" ]]; then
-		echo 'Installing in progress'
-	else
-		echo 'fail wrong password'
-		exit 1
-	fi
-else
-	echo 'Installing in progress'
-fi
 
 cp -r Config/* $config
-cp README.md $config/../EPICONF_HELPER.md
-rm $password_file
 
 find $config -type f -name "*.sh" -exec chmod +x {} \;
 chmod +x ~/afs/.confs/config/rofi/rofi-power-menu
-
-read -p "Do you want a password for protect your config files? (y/[n])" WANTED
-
-if [[ "$WANTED" == "Y" || "$WANTED" == "y" ]]; then
-	read -sp "New Password         : " PASSWORD
-	echo
-	read -sp "Confirm New Password : " CONFIRM
-	echo
-
-	if [[ "$PASSWORD" == "$CONFIRM" ]]; then
-		HASH_PASSWORD=$(hash_sha256 "$PASSWORD")
-		touch $password_file
-		echo "$HASH_PASSWORD" > $password_file
-
-	else
-		echo "Wrong Confirm Password, no password will be added"
-	fi
-fi
 
 i3-msg reload
 source $config/*
