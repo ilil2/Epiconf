84fc042f1be4eaa06e0ff745f6d54122faa0ec4b
GWINNER Ilyann <108801514+ilil2@users.noreply.github.com>
2026-06-01 18:33:37 +0200

    Update config.ini file (#17)
    * add: update config file
    
    Add function for update config.ini on update-conf
    
    * add: dev uptdate config file
    
    Add config file update in dev update
    
    * fix: update conf
    
    Fix config dev file
    
    * fix: update conf
    
    Fix update config file un dev update conf
    
    * fix: update conf
    
    Fix update config file un update conf
    
    * fix: update conf
    
    Do not get dev branch config.ini

--- Config/epiconf/update-confs.sh
+++ Config/epiconf/update-confs.sh
@@ -2,15 +2,122 @@
 
 dir=/tmp/epiconf
 
+update_config_file() {
+    file=/tmp/config.ini
+    config=~/afs/.confs/epiconf/config.ini
+
+    curl https://raw.githubusercontent.com/ilil2/Epiconf/dev/Config/epiconf/config.ini -o $file
+
+    declare -A ref_vars
+    declare -A sections
+    section=""
+
+    while IFS= read -r line || [ -n "$line" ]; do
+        line="${line#"${line%%[![:space:]]*}"}"
+        line="${line%"${line##*[![:space:]]}"}"
+
+        [ -z "$line" ] && continue
+        case "$line" in
+            \#*|\;*) continue ;;
+        esac
+
+        if [[ $line =~ ^\[(.*)\]$ ]]; then
+            section="${BASH_REMATCH[1]}"
+            continue
+        fi
+
+        if [[ $line =~ ^([^=]+)=(.*)$ ]]; then
+            key="${BASH_REMATCH[1]}"
+            value="${BASH_REMATCH[2]}"
+
+            key="${key%"${key##*[![:space:]]}"}"
+            key="${key#"${key%%[![:space:]]*}"}"
+            value="${value#"${value%%[![:space:]]*}"}"
+            value="${value%"${value##*[![:space:]]}"}"
+
+            value="${value%%;*}"
+            value="${value%%#*}"
+
+            value="${value#"${value%%[![:space:]]*}"}"
+            value="${value%"${value##*[![:space:]]}"}"
+
+            var="${section}_${key}"
+
+            if [ "$section" = "Polybar_left" ] || [ "$section" = "Polybar_right" ] || [ "$section" = "Polybar_center" ]; then
+                continue
+            fi
+
+            ref_vars["$var"]="$key=$value"
+        fi
+    done < "$file"
+
+    while IFS= read -r line || [ -n "$line" ]; do
+        line="${line#"${line%%[![:space:]]*}"}"
+        line="${line%"${line##*[![:space:]]}"}"
+
+        [ -z "$line" ] && continue
+        case "$line" in
+            \#*|\;*) continue ;;
+        esac
+
+        if [[ $line =~ ^\[(.*)\]$ ]]; then
+            section="${BASH_REMATCH[1]}"
+            sections["$section"]=1
+            continue
+        fi
+
+        if [[ $line =~ ^([^=]+)=(.*)$ ]]; then
+            key="${BASH_REMATCH[1]}"
+            value="${BASH_REMATCH[2]}"
+
+            key="${key%"${key##*[![:space:]]}"}"
+            key="${key#"${key%%[![:space:]]*}"}"
+            value="${value#"${value%%[![:space:]]*}"}"
+            value="${value%"${value##*[![:space:]]}"}"
+
+            value="${value%%;*}"
+            value="${value%%#*}"
+
+            value="${value#"${value%%[![:space:]]*}"}"
+            value="${value%"${value##*[![:space:]]}"}"
+
+            var="${section}_${key}"
+
+            if [ "$section" = "Polybar_left" ] || [ "$section" = "Polybar_right" ] || [ "$section" = "Polybar_center" ]; then
+                continue
+            fi
+
+            if [ -n "${ref_vars[$var]}" ]; then
+                unset ref_vars["$var"]
+            fi
+        fi
+    done < "$config"
+
+    for var in "${!ref_vars[@]}"; do
+        section="${var%%_*}"
+        variable="${ref_vars["$var"]}"
+
+        if [ -n "${sections[$section]}" ]; then
+            sed -i "/^\[$section\]$/a\\
+$variable" "$config"
+        else
+            echo -e "\n[$section]\n$variable" >> "$config"
+            sections[$section]=1
+        fi
+    done
+}
+
 git clone https://github.com/ilil2/Epiconf.git $dir
 rm $dir/Config/epiconf/config.ini
 if [ -d ~/afs/.confs/config/wallpapers ]; then
-	rm -r $dir/Config/config/wallpapers
+    rm -r $dir/Config/config/wallpapers
 fi
 
 cp -rT $dir/Config ~/afs/.confs/
 
 rm -rf $dir
+
+update_config_file
 
 vim +PluginInstall +qal
 (~/afs/.confs/pkgs.sh &> /dev/null & disown)
--- dev-update-conf.sh
+++ dev-update-conf.sh
@@ -1,17 +1,128 @@
 #!/bin/sh
 
+update_config_file() {
+    file=/tmp/config.ini
+    config=~/afs/.confs/epiconf/config.ini
+
+    cp Config/epiconf/config.ini $file
+
+    declare -A ref_vars
+    declare -A sections
+    section=""
+
+    while IFS= read -r line || [ -n "$line" ]; do
+        line="${line#"${line%%[![:space:]]*}"}"
+        line="${line%"${line##*[![:space:]]}"}"
+
+        [ -z "$line" ] && continue
+        case "$line" in
+            \#*|\;*) continue ;;
+        esac
+
+        if [[ $line =~ ^\[(.*)\]$ ]]; then
+            section="${BASH_REMATCH[1]}"
+            continue
+        fi
+
+        if [[ $line =~ ^([^=]+)=(.*)$ ]]; then
+            key="${BASH_REMATCH[1]}"
+            value="${BASH_REMATCH[2]}"
+
+            key="${key%"${key##*[![:space:]]}"}"
+            key="${key#"${key%%[![:space:]]*}"}"
+            value="${value#"${value%%[![:space:]]*}"}"
+            value="${value%"${value##*[![:space:]]}"}"
+
+            value="${value%%;*}"
+            value="${value%%#*}"
+
+            value="${value#"${value%%[![:space:]]*}"}"
+            value="${value%"${value##*[![:space:]]}"}"
+
+            var="${section}_${key}"
+
+            if [ "$section" = "Polybar_left" ] || [ "$section" = "Polybar_right" ] || [ "$section" = "Polybar_center" ]; then
+                continue
+            fi
+
+            ref_vars["$var"]="$key=$value"
+        fi
+    done < "$file"
+
+    while IFS= read -r line || [ -n "$line" ]; do
+        line="${line#"${line%%[![:space:]]*}"}"
+        line="${line%"${line##*[![:space:]]}"}"
+
+        [ -z "$line" ] && continue
+        case "$line" in
+            \#*|\;*) continue ;;
+        esac
+
+        if [[ $line =~ ^\[(.*)\]$ ]]; then
+            section="${BASH_REMATCH[1]}"
+            sections["$section"]=1
+            continue
+        fi
+
+        if [[ $line =~ ^([^=]+)=(.*)$ ]]; then
+            key="${BASH_REMATCH[1]}"
+            value="${BASH_REMATCH[2]}"
+
+            key="${key%"${key##*[![:space:]]}"}"
+            key="${key#"${key%%[![:space:]]*}"}"
+            value="${value#"${value%%[![:space:]]*}"}"
+            value="${value%"${value##*[![:space:]]}"}"
+
+            value="${value%%;*}"
+            value="${value%%#*}"
+
+            value="${value#"${value%%[![:space:]]*}"}"
+            value="${value%"${value##*[![:space:]]}"}"
+
+            var="${section}_${key}"
+
+            if [ "$section" = "Polybar_left" ] || [ "$section" = "Polybar_right" ] || [ "$section" = "Polybar_center" ]; then
+                continue
+            fi
+
+            if [ -n "${ref_vars[$var]}" ]; then
+                unset ref_vars["$var"]
+            fi
+        fi
+    done < "$config"
+
+    for var in "${!ref_vars[@]}"; do
+        section="${var%%_*}"
+        variable="${ref_vars["$var"]}"
+
+        if [ -n "${sections[$section]}" ]; then
+            sed -i "/^\[$section\]$/a\\
+$variable" "$config"
+        else
+            echo -e "\n[$section]\n$variable" >> "$config"
+            sections[$section]=1
+        fi
+    done
+}
+
 git pull
 mkdir ~/epiconf
+
 cp -r ./* ~/epiconf/
 rm ~/epiconf/Config/epiconf/config.ini
 if [ -d ~/afs/.confs/config/wallpapers ]; then
 	rm -r ~/epiconf/Config/config/wallpapers
 fi
+
 cp -rT ~/epiconf/Config ~/afs/.confs/
 cp ~/epiconf/README.md ~/afs/EPICONF_HELPER.md
 rm -rf ~/epiconf
+
+update_config_file
+
 chmod +x ~/afs/.confs/config/rofi/rofi-power-menu
 vim +PluginInstall +qal
 (~/afs/.confs/pkgs.sh &> /dev/null & disown)
 sh ~/afs/.confs/install.sh
+
 echo "EpiConf dev updated !"
