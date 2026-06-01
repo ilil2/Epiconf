b472e84d258a5e12482d0032f364b8997e5ea2b7
ilyann.gwinner <ilyann.gwinner@gmail.com>
2026-06-01 16:19:57 +0200

    add: issue alias
    Add issue alias for show epiconf issue pages

--- Config/bash/epiconf.sh
+++ Config/bash/epiconf.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 alias epiconf='firefox https://github.com/ilil2/Epiconf/wiki'
+alias issue='firefox https://github.com/ilil2/Epiconf/issues/new'
 alias update-conf='~/afs/.confs/epiconf/update-confs.sh'
 alias reset-conf='curl -L https://raw.githubusercontent.com/Darkrentin/default-nixos-config-epita/refs/heads/main/installer.sh | sh -s && cp ~/afs/.confs/config/i3/config ~/config.temp && rm ~/afs/.confs/config/i3/* && mv ~/config.temp ~/afs/.confs/config/i3/config && rm -r ~/afs/.confs/epiconf && rm ~/afs/.confs/pkgs.sh && i3-msg restart && pkill polybar && pkill picom'
 
