c7cc39adc450e63e1257a075d9164216ee68e863
ilyann.gwinner <ilyann.gwinner@gmail.com>
2026-06-01 16:09:43 +0200

    fix: epiconf aliases
    Change epiconf and reset-conf aliases

--- Config/bash/epiconf.sh
+++ Config/bash/epiconf.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-alias epiconf='mdcat ~/afs/EPICONF_HELPER.md'
+alias epiconf='firefox https://github.com/ilil2/Epiconf/wiki'
 alias update-conf='~/afs/.confs/epiconf/update-confs.sh'
-alias reset-conf='curl -L https://raw.githubusercontent.com/Darkrentin/default-nixos-config-epita/refs/heads/main/installer.sh | sh -s && cp ~/afs/.confs/config/i3/config ~/config.temp && rm ~/afs/.confs/config/i3/* && mv ~/config.temp ~/afs/.confs/config/i3/config && rm -r ~/afs/.confs/epiconf && rm ~/afs/EPICONF_HELPER.md && rm ~/afs/.confs/pkgs.sh && rm ~/afs/.confs/.user && i3-msg restart && pkill polybar && pkill picom'
+alias reset-conf='curl -L https://raw.githubusercontent.com/Darkrentin/default-nixos-config-epita/refs/heads/main/installer.sh | sh -s && cp ~/afs/.confs/config/i3/config ~/config.temp && rm ~/afs/.confs/config/i3/* && mv ~/config.temp ~/afs/.confs/config/i3/config && rm -r ~/afs/.confs/epiconf && rm ~/afs/.confs/pkgs.sh && i3-msg restart && pkill polybar && pkill picom'
 
 alias config='$EDITOR ~/afs/.confs/epiconf/config.ini'
 
