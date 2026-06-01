d5ffbb8b99bea7bfbe8fafeefd9403193e144c6f
Ilyann gwinner <ilyann.gwinner@epita.fr>
2026-06-01 18:31:14 +0200

    fix: update conf
    Do not get dev branch config.ini

--- dev-update-conf.sh
+++ dev-update-conf.sh
@@ -4,7 +4,7 @@ update_config_file() {
     file=/tmp/config.ini
     config=~/afs/.confs/epiconf/config.ini
 
-    curl https://raw.githubusercontent.com/ilil2/Epiconf/main/Config/epiconf/config.ini -o $file
+    cp Config/epiconf/config.ini $file
 
     declare -A ref_vars
     declare -A sections
